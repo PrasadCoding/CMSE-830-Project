c82b660b0e300665d2f2f16ce96d08f4dda5c65a
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2024-10-16 16:58:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Hello World')
 
 st.write('Hello world!')
-
+st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
 st.sidebar.success("Select a page")
