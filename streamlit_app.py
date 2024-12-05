ec572b91beaf6a04804b3978b0255a3c389e73a7
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2024-12-04 22:37:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,28 @@
 import streamlit as st
-from PIL import Image
-import os
+import base64
+from pathlib import Path
 
 # Set the page layout to wide
 st.set_page_config(page_title="Heart Disease Prediction", page_icon="❤️", layout="wide")
 
-# Set background image using a relative path
+# Function to load an image for background
 def set_bg_image(image_path):
     """
     Set background image for the Streamlit app
     """
+    # Load the image as binary
+    with open(image_path, "rb") as image_file:
+        img_bytes = image_file.read()
+
+    # Encode the image to base64
+    encoded_image = base64.b64encode(img_bytes).decode()
+
+    # Use the base64 encoded image for the background
     st.markdown(
         f"""
         <style>
         .stApp {{
-            background-image: url("{image_path}");
+            background-image: url('data:image/png;base64,{encoded_image}');
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
@@ -25,8 +33,8 @@ def set_bg_image(image_path):
         unsafe_allow_html=True,
     )
 
-# Path to the image in the 'images' folder
-image_path = 'images/bg4_o.png'  # Replace with your actual image name
+# Path to your image file
+image_path = Path('images/bg_4.png')  # Replace with your actual image file name
 set_bg_image(image_path)
 
 # Add title to the app
