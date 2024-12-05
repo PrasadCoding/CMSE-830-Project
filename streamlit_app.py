93168256d69b5ab2847221cb61e257b67a5bc745
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2024-12-04 23:55:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,8 +69,11 @@ st.markdown(
         text-align: left;  /* Align the bullet points to the left */
         max-width: 800px;
         margin: 0 auto;
-        font-size: 28px;  /* Increased font size */
+        font-size: 24px;  /* Increased font size */
         color: #000000;  /* Bullet points in black */
+    }
+    li {
+        font-size: 28px
     }
     </style>
     """,
