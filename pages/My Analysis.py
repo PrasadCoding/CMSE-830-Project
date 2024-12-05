051c6fe12722f8a414614a0eaccc51daf5bc6783
Prasad Upasani <89271401+PrasadCoding@users.noreply.github.com>
2024-12-05 06:43:30 -0500

    Update My Analysis.py

--- pages/My Analysis.py
+++ pages/My Analysis.py
@@ -5,6 +5,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 
+def set_bg_image(image_url):
+    """
+    Set background image for the Streamlit app using a raw GitHub URL
+    """
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("{image_url}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+# Path to your background image file on GitHub (use the raw URL)
+image_url = 'https://raw.githubusercontent.com/PrasadCoding/CMSE-830-Project/refs/heads/master/images/bg43.png'  # Replace with your raw URL
+set_bg_image(image_url)
+
 df = pd.read_csv("dataset/heart_disease.csv")
 # Dataset description section
 st.title("My Analysis")
